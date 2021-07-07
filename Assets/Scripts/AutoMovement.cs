bd889f8d50c054a17244cf71b1385316621a6436
Chau56 <56555237+Chau56@users.noreply.github.com>
2021-07-07 11:46:50 +0800

    功能01-1
    方块自动化往前移动（添加一个浅光色拖尾）
    2021年7月7日11:46更新

--- Assets/Scripts/AutoMovement.cs
+++ Assets/Scripts/AutoMovement.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class AutoMovement : MonoBehaviour
 {
-    [SerializeField] public static float speed = 5f;      //物体移动速度
+    Rigidbody2D myRigidbody;
+    [SerializeField] public float speed = 5f;      //方块移动速度
+    [SerializeField] public bool canMove = true;    //控制方块暂停
 
-    // Update is called once per frame
     void Update()
     {
-        //遍历背景，获取子物体（方块A和B）
-        foreach (Transform tran in transform)           //tran是Background层下的所有子物体
+        if (canMove == true)
         {
-            //获取子物体的位置
-            Vector3 pos = tran.position;
-
-            //按照速度向右侧移动
-            pos.x += speed * Time.deltaTime;
-
-            //位置赋给子物体
-            tran.position = pos;
+            myRigidbody.transform.Translate(speed * Time.deltaTime, 0, 0);  
         }
+    }
+
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody2D>();
     }
 }
