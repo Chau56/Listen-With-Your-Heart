df71bd59c097541d5b0e83d0f3f1e21e4ad3f23b
Chau56 <56555237+Chau56@users.noreply.github.com>
2021-07-19 20:42:52 +0800

    功能3-4
    添加Bonus的计算

---
+++ Assets/Scripts/BonusShow.cs
@@ -0,0 +1,30 @@
+///<summary>
+///作者：周澄鑫
+///创建日期：2021-7-19
+///更新者：周权
+///最新修改日期：2021-7-19
+///</summary>
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BonusShow : MonoBehaviour
+{
+    [SerializeField]
+    private Distance d;
+    private Text BonusScore;
+
+    private void Start()
+    {
+        BonusScore = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        BonusScore.text = d.Bonus.ToString();
+    }
+
+}
--- Assets/Scripts/PerfectCollisionParticle.cs
+++ Assets/Scripts/PerfectCollisionParticle.cs
@@ -1,8 +1,8 @@
 ///<summary>
-///作者：张翔宇
+///作者：周澄鑫
 ///创建日期：2021-7-16
 ///更新者：周权
-///最新修改日期：2021-7-17
+///最新修改日期：2021-7-19
 ///</summary>
 
 
@@ -14,25 +14,39 @@ public class PerfectCollisionParticle : MonoBehaviour
     private ParticleSystem whitePerfectCollision, blackPerfectCollision;
     [SerializeField]
     private Rigidbody2D whiteBlock, blackBlock;
+    [SerializeField]
+    private Distance d1;
+    [SerializeField]
+    private Distance d2;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var obj = collision.gameObject;
-        //if (collision.gameObject.CompareTag("White") && blackX == whiteX)         //由于运动过程会产生前后位移差，故不能直接判断是否完全相等（Bug13导致的）
-        //if (collision.gameObject.CompareTag("White") && Mathf.Abs((whiteY + blackY) / 2 - halfScreen) <= halfScreen * 0.01)
         if (obj.CompareTag("White") || obj.CompareTag("Black"))
         {
-            float whiteY = whiteBlock.velocity.y;
-            float blackY = blackBlock.velocity.y;
-
             //两方块旋转交换位置
             ChangePosition();
 
             //完美碰撞时触发粒子特效
-            PerfectParticle(whiteY, blackY);
+            PerfectParticle();
+
+            //进入Bonus状态时，记录当前双方距离大小
+            d1.StartBonus();
+            d2.StartBonus();
         }
-        else
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        var obj = collision.gameObject;
+        if (obj.CompareTag("White") || obj.CompareTag("Black"))
         {
+            //退出Bonus状态时，再次记录双方距离大小
+            d1.EndBonus();
+            d2.EndBonus();
+
+            //停止播放完美碰撞粒子特效
             whitePerfectCollision.Stop();
             blackPerfectCollision.Stop();
         }
@@ -40,11 +54,11 @@ public class PerfectCollisionParticle : MonoBehaviour
 
     private void ChangePosition()
     {
-
         //交换位置
         var pos = whiteBlock.transform.position;
         whiteBlock.transform.position = blackBlock.transform.position;
         blackBlock.transform.position = pos;
+
         //将方块竖直方向速度清零
         ClearY(blackBlock);
         ClearY(whiteBlock);
@@ -56,7 +70,7 @@ public class PerfectCollisionParticle : MonoBehaviour
         body.velocity = new Vector2(body.velocity.x, 0);
     }
 
-    private void PerfectParticle(float whiteY, float blackY)
+    private void PerfectParticle()
     {
         //从自己区域触发完美碰撞时播放粒子特效，反之不触发
         whitePerfectCollision.Play();
