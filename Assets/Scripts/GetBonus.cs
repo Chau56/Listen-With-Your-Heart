564c0f8b3b80484aabde7fc4268b846240fc9608
bander972 <1628580708@qq.com>
2021-07-21 09:49:14 +0800

    增长功能，距离和分数在GetScore,GetBonus中，重写了部分方法，等待整合

--- Assets/Scripts/GetBonus.cs
+++ Assets/Scripts/GetBonus.cs
@@ -4,7 +4,7 @@
 ///最新修改日期：2021-7-15
 ///</summary>
 
-
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,15 +14,33 @@ public class GetBonus : MonoBehaviour
     private Text text;
     [SerializeField]
     private Distance distance;
-
+    private int maxValue;
+    private int minValue;
+    private int Times;
     private void Start()
     {
         text = GetComponent<Text>();
         GameEvents.instance.GameWin += Renew;
+        
     }
-
     private void Renew()
     {
-        text.text = $"+{distance.Bonus}";
+       
     }
+    public IEnumerator Bonus()
+    {
+        maxValue = distance.Bonus;
+        minValue = 0;
+        Times = distance.Bonus;
+        int result  = minValue;
+        for (int i = 0; i < Times; i++)
+        {
+            result++;
+            text.text = $"+{result}";
+            yield return new WaitForSeconds(0.01f);
+        }
+        text.text = $"+{maxValue}";
+        StopCoroutine(Bonus());
+    }
+   
 }
