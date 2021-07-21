004b71b287a895462c423c0160d82f2139d4cb49
TlMegalovania <tlmegalovania@qq.com>
2021-07-21 16:03:56 +0800

    fix：现在暂停时不能再点击暂停按钮

--- Assets/Scripts/PauseBackground.cs
+++ Assets/Scripts/PauseBackground.cs
@@ -6,16 +6,19 @@
 
 
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Animator))]
 public class PauseBackground : MonoBehaviour
 {
     private GameEvents events;
     private Animator anima;
+    private Image image;
     // Start is called before the first frame update
     private void Start()
     {
         events = GameEvents.instance;
+        image = GetComponent<Image>();
         anima = GetComponent<Animator>();
         events.GamePause += Pause;
         events.GameResume += Resume;
@@ -26,10 +29,12 @@ public class PauseBackground : MonoBehaviour
     private void Pause()
     {
         anima.SetBool("Pause", true);
+        image.raycastTarget = true;
     }
 
     private void Resume()
     {
         anima.SetBool("Pause", false);
+        image.raycastTarget = false;
     }
 }
