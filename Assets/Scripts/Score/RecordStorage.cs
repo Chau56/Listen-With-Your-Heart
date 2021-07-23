792a6dc2bc5f177e45c7bd3df4243c3c9bea2b0b
bander972 <1628580708@qq.com>
2021-07-23 11:21:18 +0800

    增加新纪录功能，可以测试

--- Assets/Scripts/Score/RecordStorage.cs
+++ Assets/Scripts/Score/RecordStorage.cs
@@ -16,7 +16,7 @@
 
 using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class RecordStorage : MonoBehaviour
 {
     [SerializeField]
@@ -27,16 +27,23 @@ public class RecordStorage : MonoBehaviour
     private Distance D1;
     [SerializeField]
     private Distance D2;
-
+    [SerializeField]
+    private GameObject whiteRecord;
+    [SerializeField]
+    private GameObject blackRecord;
+    private Image white;
+    private Image black;
+    private int score = 0;
     private void StoreScore()
     {
         StartCoroutine(Store());
     }
 
+
     private IEnumerator Store()
     {
         yield return new WaitForEndOfFrame();
-        int score = D1.Bonus + D1.Value + D2.Bonus + D2.Value;
+        int score = 2 * D1.Value - D1.Bonus + 2 * D2.Value - D2.Bonus;
         if (score <= PlayerPrefs.GetInt("Score", -1)) yield break;
         PlayerPrefs.SetInt("BlackDistance", D1.Value);
         PlayerPrefs.SetInt("WhiteDistance", D2.Value);
@@ -44,13 +51,33 @@ public class RecordStorage : MonoBehaviour
         PlayerPrefs.SetInt("WhiteRate", P2.Percent);
         PlayerPrefs.SetInt("Score", score);
     }
-
+    private void ResetRecord()
+    {
+        black.enabled = false;
+        white.enabled = false;
+    }
+    private void show()
+    {
+        score = 2 * D1.Value - D1.Bonus + 2 * D2.Value - D2.Bonus;
+        if (score > PlayerPrefs.GetInt("Score", -1))
+        {
+            black.enabled = true;
+            white.enabled = true;
+        }
+    }
     // Start is called before the first frame update
     private void Start()
     {
+        white = whiteRecord.GetComponent<Image>();
+        black = blackRecord.GetComponent<Image>();
+        white.enabled = false;
+        black.enabled = false;
         var events = GameEvents.instance;
+        events.GameRestart += ResetRecord;
+        events.GameWin += show;
         events.GameEnd += StoreScore;
         events.GameBeforeAwake += debugTest;
+        PlayerPrefs.DeleteAll();
     }
 
     private void debugTest()
